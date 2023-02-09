04bc788098d90a77cc4308562ec0c8f9c08fde7a
lucas <lucas.thomaas43@gmail.com>
2023-02-09 20:53:26 -0300

    mi primer commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs')
+const mainRouter = require('./routes/mainRoutes')
+const path = require('path')
+app.listen(3000, console.log('estamos corriendo en el puerto 3000'))
+
+//template engine EJS
+app.set('view engine', 'ejs')
+app.set('views', './views')
+//archivos estaticos
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+//rutas Main
+app.use('/', mainRouter)
+
+//static
+//static
---
+++ src/controllers/mainControllers.js
@@ -0,0 +1,7 @@
+
+
+
+module.exports = mainControllers = {
+    index : (req, res)=>{res.render('index')}
+    
+}
